5c61ed5daa31f474d9f4897975c6961284301424
jamesmaino <jamesmaino@gmail.com>
2019-06-10 14:11:38 +1000

    new post on ROC and AUC

--- content/post/data/useful_scripts/mydarktheme.R
+++ content/post/data/useful_scripts/mydarktheme.R
@@ -3,6 +3,7 @@ library(ggplot2)
 mydarktheme = 
   theme_minimal() +
   theme(
+    panel.background = element_rect(fill = rgb(.2,.21,.27)),
     plot.background = element_rect(fill = rgb(.2,.21,.27)),
     text = element_text(colour = 'grey'), 
     axis.text = element_text(colour = 'grey'), 
